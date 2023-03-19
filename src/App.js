d607e81e80467d35e3ef2aeb61e0714c5708fe34
Manish Yadav <mkdablu@gmil.com>
2023-03-20 01:16:11 +0530

    Changes on app.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         New Document React
         </a>
       </header>
     </div>
